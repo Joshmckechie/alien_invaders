ac02a6f6bc06b7677006c0097cfec4a3fd518f64
Josh McKechie <josh.mckechie@gmail.com>
2014-05-20 11:27:16 +0100

    Missile changed
    I changed the missile to a green lightsaber.

--- js/level.js
+++ js/level.js
@@ -44,7 +44,7 @@
     'alien1': { sx: 0,  sy: 0,  w: 23, h: 18, cls: Alien, frames: 2 },
     'alien2': { sx: 0,  sy: 18, w: 23, h: 18, cls: Alien, frames: 2 },
     'player': { sx: 0,  sy: 36, w: 26, h: 17, cls: Player },
-    'missile': { sx: 0,  sy: 86, w: 3,  h: 14, cls: Missile }
+    'missile': { sx: 86,  sy: 34, w: 5,  h: 11, cls: Missile }
   }
 
  		//This is the start page for the game itself.
